872afffe7bb3f04ee13380f44e9b38b8fc298f3a
Damien Robinson <damien.robinson@griffithuni.edu.au>
2018-03-13 16:46:26 +1000

    Enhanced MathPlugin with trigonometrical functions (sine, cosine, and tangent).
    Added 3 trignometrical functions to the custom plugin.

--- Math Plugin/VueMathPlugin.js
+++ Math Plugin/VueMathPlugin.js
@@ -6,5 +6,15 @@ export default {
     Vue.directive('sqrt', function (el, binding) { 
       el.innerHTML = Math.sqrt(binding.value); 
     }); 
+	Vue.directive('sine', function (el, binding) { 
+      el.innerHTML = Math.sin(binding.value); 
+    });
+	Vue.directive('cosine', function (el, binding) { 
+      el.innerHTML = Math.cos(binding.value); 
+    });
+	Vue.directive('tangent', function (el, binding) { 
+      el.innerHTML = Math.tan(binding.value); 
+    });
   } 
 }; 
+
--- Math Plugin/main.js
+++ Math Plugin/main.js
@@ -12,6 +12,15 @@ exports.default = {
     Vue.directive('sqrt', function (el, binding) {
       el.innerHTML = Math.sqrt(binding.value);
     });
+    Vue.directive('sine', function (el, binding) {
+      el.innerHTML = Math.sin(binding.value);
+    });
+    Vue.directive('cosine', function (el, binding) {
+      el.innerHTML = Math.cos(binding.value);
+    });
+    Vue.directive('tangent', function (el, binding) {
+      el.innerHTML = Math.tan(binding.value);
+    });
   }
 };
 
