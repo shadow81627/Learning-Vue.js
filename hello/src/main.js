00b3af34ade028555de23e407f7d68e6b9cc59f7
shadow81627 <damien.robinson@griffithuni.edu.au>
2018-04-07 17:37:02 +1000

    Added Hello Component
    The hello component is a vue file which contains all of the code for the component. The component is then added to the index html using its name hello-component tag. The component also needs to be loaded in the main js file. The componenet is imported in the main js file and listed in the components for the app.

--- hello/src/main.js
+++ hello/src/main.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+import Vue from 'vue' 
+import HelloComponent from './HelloComponent.vue' 
+ 
+new Vue({ 
+  el: '#app', 
+  components: { HelloComponent } 
+}); 
