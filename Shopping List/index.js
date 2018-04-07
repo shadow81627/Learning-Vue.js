79a010d57a16435fd630b677043497f620e1200d
shadow81627 <damien.robinson@griffithuni.edu.au>
2018-04-07 15:36:22 +1000

    Rewriting the shopping list with simple components
    The shopping list app has been changed to use vue components and templates.

--- Shopping List/index.js
+++ Shopping List/index.js
@@ -1,70 +1,49 @@
-var data = { 
-  items: [{ text: 'Bananas', checked: true },
-          { text: 'Apples', checked: false }], 
-  title: 'My Shopping List', 
-  newItem: '' 
-}; 
- 
-/** 
- * Declaring components 
- */ 
-var ItemsComponent = Vue.extend({ 
-  data: function () { 
-    return data; 
-  }, 
-  template: '<ul>' + 
-  '           <li v-for="item in items" :class="{ \'removed\': item.checked }">' + 
-  '             <div class="checkbox">' + 
-  '               <label>' + 
-  '                 <input type="checkbox" v-model="item.checked"> {{ item.text }}' + 
-  '               </label>' + 
-  '             </div>' + 
-  '           </li>' + 
-  '         </ul>' 
-}); 
-var ChangeTitleComponent = Vue.extend({ 
-  data: function () { 
-    return data; 
-  }, 
-  template: '<input v-model="title"/>' 
-}); 
-var AddItemComponent = Vue.extend({ 
-  data: function () { 
-    return data; 
-  }, 
+var data = {
+  items: [{ text: "Bananas", checked: true }, { text: "Apples", checked: false }],
+  title: "My Shopping List",
+  newItem: ""
+};
+//add item compoennt
+Vue.component("add-item-component", {
+  template: "#add-item-template",
+  props: ["items", "newItem"]
+});
+//item component
+Vue.component("item-component", {
+  template: "#item-template",
+  props: ["item"]
+});
+//items component
+Vue.component("items-component", {
+  template: "#items-template",
+  props: ["items"]
+});
+//change title component
+Vue.component("change-title-component", {
+  template: "#change-title-template",
+  props: ["title", "value"],
   methods: { 
-    addItem: function () { 
-      var text; 
- 
-      text = this.newItem.trim(); 
-      if (text) { 
-        this.items.push({ 
-          text: text, 
-          checked: false 
-        }); 
-        this.newItem = ""; 
-      } 
+    onInput: function (event) { 
+      this.$emit('input', event.target.value) 
     } 
-  }, 
-  template: 
-  '<div class="input-group">' + 
-    '<input v-model="newItem" @keyup.enter="addItem" placeholder="add shopping list item" type="text" class="form-control">'  + 
-    '<span class="input-group-btn">'  + 
-    '  <button @click="addItem" class="btn btn-default" type="button">Add!</button>'  + 
-    '</span>'  + 
-  '</div>' 
-}); 
- 
-/** 
- * Registering components 
- */ 
-Vue.component('items-component', ItemsComponent); 
-Vue.component('change-title-component', ChangeTitleComponent); 
-Vue.component('add-item-component', AddItemComponent); 
-/** 
- * Instantiating a Vue instance 
- */ 
-new Vue({ 
-  el: '#app', 
-  data: data 
-}); 
+  }
+});
+
+new Vue({
+  el: "#app",
+  data: data,
+  methods: {
+    addItem: function () {
+      var text;
+
+      text = this.newItem.trim();
+      if (text) {
+        this.items.push({
+          text: text,
+          checked: false
+        });
+        this.newItem = "";
+      }
+    }
+  }
+});
