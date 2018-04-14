b2f6e07cfa5b8fc430c124b22bd63ce87e9a9452
shadow81627 <damien.robinson@griffithuni.edu.au>
2018-04-14 16:20:15 +1000

    Added active class
    Added active class to navigation list and shoppinglist component. The class is conditionally added if the element is first in the array.

--- shopping-list/src/App.vue
+++ shopping-list/src/App.vue
@@ -1,12 +1,12 @@
 <template>
 <div id="app" class="container">
   <ul class="nav nav-tabs" role="tablist">
-    <li v-for="list in shoppinglists" v-bind:key="list.id" role="presentation">
+    <li v-bind:class= "index===0 ? 'active' : ''" v-for="(list, index) in shoppinglists" v-bind:key="list.id" role="presentation">
       <a v-bind:href="'#' + list.id" v-bind:aria-controls="list.id" role="tab" data-toggle="tab">{{ list.title }}</a>
     </li>
   </ul>
   <div class="tab-content">
-    <div v-for="list in shoppinglists" v-bind:key="list.id" class="tab-pane" role="tabpanel" v-bind:id="list.id">
+    <div v-bind:class= "index===0 ? 'active' : ''" v-for="(list, index) in shoppinglists" v-bind:key="list.id" class="tab-pane" role="tabpanel" v-bind:id="list.id">
       <shopping-list-component v-bind:title="list.title" v-bind:items="list.items"></shopping-list-component>
     </div>
   </div>
