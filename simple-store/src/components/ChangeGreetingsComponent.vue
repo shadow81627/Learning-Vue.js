668a74dbc722ee09371af12bd44c68aba1f4c6a0
shadow81627 <damien.robinson@griffithuni.edu.au>
2018-04-15 21:51:02 +1000

    Added handleMessageInputChanges
    Added handleMessageInputChanges to proccess both the change message event and increment counter event in a single mutation. This allows us to cut down on the number of events being called on the input.

--- simple-store/src/components/ChangeGreetingsComponent.vue
+++ simple-store/src/components/ChangeGreetingsComponent.vue
@@ -1,14 +1,13 @@
 <template>
-  <input :value="msg" @keyup="changeMessage($event.target.value)" @keyup.enter="incrementCounter">
+  <input :value="msg" @keyup="handleMessageInputChanges($event)">
 </template>
 <script>
-  import { mapGetters } from 'vuex'
-  import { mapActions } from 'vuex'
+  import { mapGetters, mapActions } from 'vuex'
 
   export default {
     computed: mapGetters({
       msg: 'getMessage'
     }),
-    methods:  mapActions(['changeMessage', 'incrementCounter'])
+    methods:  mapActions(['handleMessageInputChanges'])
   }
 </script>
