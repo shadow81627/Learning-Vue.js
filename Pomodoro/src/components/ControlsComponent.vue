db5830a6ec84e69498b3b9335f8d0f7858288668
shadow81627 <damien.robinson@griffithuni.edu.au>
2018-04-21 14:56:37 +1000

    Added actions to controls component

--- Pomodoro/src/components/ControlsComponent.vue
+++ Pomodoro/src/components/ControlsComponent.vue
@@ -1,23 +1,24 @@
 <template>
 <h2>
   <span>{{ 'Pomodoro' | lowercase | uppercase | addspace }}</span>
-  <button :disabled='isStarted && !isPaused'>
+  <button :disabled='isStarted && !isPaused' @click="start">
     <i class="glyphicon glyphicon-play"></i>
   </button>
-  <button :disabled='!isStarted || isPaused'>
+  <button :disabled='!isStarted || isPaused' @click="pause">
     <i class="glyphicon glyphicon-pause"></i>
   </button>
-  <button :disabled='!isStarted'>
+  <button :disabled='!isStarted' @click="stop">
    <i class="glyphicon glyphicon-stop"></i>
   </button>
 </h2>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
-  computed: mapGetters(['isStarted', 'isPaused', 'isStopped'])
+  computed: mapGetters(['isStarted', 'isPaused', 'isStopped']),
+  methods: mapActions(['start', 'stop', 'pause'])
 }
 </script>
 
