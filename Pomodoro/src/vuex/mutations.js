a9a03ab671b8e042b34c43c6ffd753f166dca905
shadow81627 <damien.robinson@griffithuni.edu.au>
2018-04-22 21:12:51 +1000

    Added togglePromodoro function
    Added togglePromodoro function for changing the counter state between working and resting time based on the given toggle paramater.

--- Pomodoro/src/vuex/mutations.js
+++ Pomodoro/src/vuex/mutations.js
@@ -1,17 +1,27 @@
 import * as types from './mutation_types'
+import _ from 'underscore'
+import { WORKING_TIME, RESTING_TIME } from '../config'
+
+function togglePomodoro (state, toggle) {
+  if (_.isBoolean(toggle) === false) {
+    toggle = !state.isWorking
+  }
+  state.isWorking = toggle
+  state.counter = state.isWorking ? WORKING_TIME : RESTING_TIME
+}
 
 export default {
-  [types.START](state) {
+  [types.START] (state) {
     state.started = true
     state.paused = false
     state.stopped = false
   },
-  [types.PAUSE](state) {
+  [types.PAUSE] (state) {
     state.paused = true
     state.started = true
     state.stopped = false
   },
-  [types.STOP](state) {
+  [types.STOP] (state) {
     state.stopped = true
     state.paused = false
     state.started = false
