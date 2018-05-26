48c2d9d1e6ee44b07086344d02d634ebf1920101
shadow81627 <damien.robinson@griffithuni.edu.au>
2018-05-26 13:14:03 +1000

    Added white space between function name and parentheses

--- shopping-list/src/vuex/mutations.js
+++ shopping-list/src/vuex/mutations.js
@@ -1,15 +1,15 @@
 import _ from 'underscore'
 import * as types from './mutation_types'
 
-function findById(state, id) {
+function findById (state, id) {
   return _.findWhere(state.shoppinglists, { id: id })
 }
 
 export default {
-  [types.CHANGE_TITLE](state, data) {
+  [types.CHANGE_TITLE] (state, data) {
     findById(state, data.id).title = data.title
   },
-  [types.POPULATE_SHOPPING_LISTS](state, lists) {
+  [types.POPULATE_SHOPPING_LISTS] (state, lists) {
     state.shoppinglists = lists
   }
 }
